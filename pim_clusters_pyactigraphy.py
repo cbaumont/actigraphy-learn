1f2a7b393fa3889723316fccb24ba9a6f83b61a0
Carlos Baumont <baumont.carlos@gmail.com>
2023-12-03 12:13:06 +0000

    Update gitignore

--- pim_clusters_pyactigraphy.py
+++ pim_clusters_pyactigraphy.py
@@ -5,4 +5,3 @@ import matplotlib.pyplot as plt
 fpath = "test_sample_atr.txt"
 kmeans_clustering = BaseClustering(fpath, 5, "PIM", "cluster")
 data_frame_kmeans = kmeans_clustering.kmeans_light_data('LIGHT')
-
